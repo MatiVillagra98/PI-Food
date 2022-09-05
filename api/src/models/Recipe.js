4a22c73d0bcaf792b4ace96da831bac190ebd2e3
mativillagra98 <mativillagra98@gmail.com>
2022-09-05 07:20:50 -0300

    router

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,27 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    resume: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    health_score: {
+      type: DataTypes.INTEGER,
+    },
+    preparation: {
+      type: DataTypes.TEXT
+    }
+  },
+  {
+    timestamps: false
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,56 @@
+require('dotenv').config();
 const { Router } = require('express');
+const { Op, Diet, Recipe } = require('../db');
+const axios = require('axios');
+const { API_KEY } = process.env;
+
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+
+router.get('/recipes', async (req, res) => {
+    const { name } = req.query;
+    let foodList = [];
+    await
+    axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}`)
+        .then(response => response.data)
+        .then(data => {
+            data.results.map(r => {
+                console.log(r.title)
+                if(r.title.includes(name)) foodList.push(r)
+            })
+        })
+        .catch(err => console.log(err));
+    foodList.concat(await Recipe.findAll(
+        {where: 
+            {name: {
+                [Op.like]:`%${name}%`
+            }}
+        }
+    ));
+    if(foodList.length != 0) {
+        res.status(200).json(foodList);
+    }
+    else {
+        console.log(foodList)
+        res.status(400).send('Comida no encontrado')
+    }
+});
+
+router.get('/details/recipes/:id', async (req, res) => {
+    const id = req.params;
+    const food = [];
+    if (!isNaN(id)) {
+        axios(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
+        .then(res => res.json())
+        .then(res => food = res.map())
+    }
+})
+
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
